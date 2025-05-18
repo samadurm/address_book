6ea02a03d240fa3ba6833c23f23013025fe88670
Micah Samaduroff <msamaduroff@gmail.com>
2025-05-17 17:02:41 -0700

    initial project files

---
+++ address_book/address_book.py
@@ -0,0 +1,17 @@
+from contact import Contact
+
+class AddressBook:
+    def __init__(self):
+        pass
+
+    def add(self, contact: Contact):
+        pass
+
+    def remove(self, contact: Contact):
+        pass
+
+    def remove(self, name: str):
+        pass
+
+    def find(self, name: str) -> Contact:
+        pass
