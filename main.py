20e4df1f915bb486e6b630ce510991ef8aad7024
Micah Samaduroff <msamaduroff@gmail.com>
2025-06-02 20:03:07 -0700

    WIP on input event loop

--- main.py
+++ main.py
@@ -1,5 +1,52 @@
+from address_book.address_book import AddressBook
+
+def print_contact(address_book: AddressBook):
+    print(address_book)
+
+def add_contact(address_book: AddressBook):
+    print('add contact called')
+    pass
+
+def edit_contact(address_book: AddressBook):
+    print('edit contact called')
+    pass
+
+def delete_contact(address_book: AddressBook):
+    print('delete contact called')
+    pass
+
 def main():
-    print("hello world!")
+    address_book = AddressBook()
+
+    print('Address Book')
+
+    command_map = {
+        'p': print_contact,
+        'a': add_contact,
+        'e': edit_contact,
+        'd': delete_contact
+    }
+
+    run = True
+    while run:
+        message = (f"Enter one of the following commands:\n"
+                    f"- 'p': print current contact\n"
+                    f"- 'a': add a new contact\n"
+                    f"- 'e': edit a contact\n"
+                    f"- 'd': delete a contact\n"
+                    f"- 'q': quit\n"
+        )
+
+        lowercase_input = input(message).lower()
+        if lowercase_input == 'q':
+            break
+
+        if func := command_map.get(lowercase_input):
+            func(address_book=address_book)
+        else:
+            print('Invalid command.')
+            
+
 
 if __name__ == '__main__':
     main()
